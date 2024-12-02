43db2c6c18c6258fdc84c66576bb2fa3870f5147
Iyanulawal <76182798+Iyanulawal@users.noreply.github.com>
2024-12-02 08:47:00 -0600

    Update Code2.py

---
+++ Code2.py
@@ -0,0 +1,9 @@
+#A bit more complex 
+x = "awesome" #the value of X is "Awesome"
+
+def myfunc(): #My Function
+  print("Python is " + x) #Print statement + Variable
+
+myfunc()
+#Variables that are created outside of a function
+#Known as a Global Variable 
